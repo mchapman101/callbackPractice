da641e0d5186534fd76fcd24fc7146c9d8b56389
MarkChapman <mchapman101@yahoo.com>
2016-03-30 18:57:14 -0600

    wednesday work

--- callbackPractice.js
+++ callbackPractice.js
@@ -1,16 +1,16 @@
 /* In this repo your job is to write functions to make each function call work properly.
-Below is a sample problem 
+Below is a sample problem
 
   //code here for sayHi
 
    sayHi('Hi Katie', function(thingToSay){
       alert(thingToSay);
    });
-   
 
-and what you should write is the sayHi function that makes the code above work, 
-    
-    
+
+and what you should write is the sayHi function that makes the code above work,
+
+
    var sayHi = function(str, cb){
     cb(str);
    }
@@ -18,14 +18,16 @@ and what you should write is the sayHi function that makes the code above work,
    sayHi('Hi Katie', function(thingToSay){
       alert(thingToSay); //should alert ('Hi Katie')'
    });
-    
-    
+
+
 */
 
 
 
-  //Code Here for first
-  
+function first(names, cb){
+  cb(names[0]);
+}  //Code Here for first
+
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 first(names, function(firstName){
@@ -39,7 +41,9 @@ first(names, function(firstName){
 
 
 
-  //Code Here for last
+function last(names, cb){
+  cb(names[names.length - 1]);
+}  //Code Here for last
 
 last(names, function(lastName){
   console.log('The last name in names is ' + lastName);
@@ -52,9 +56,9 @@ last(names, function(lastName){
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
 
-
-
-
+function multiply(num1, num2, cb){
+   return cb(num1 * num2);
+}
 
   //Code Here for multiply
 
@@ -67,6 +71,15 @@ multiply(4, 3, function(answer){
 
 
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+function contains(array, name, callback){    //code here
+  if (array.indexOf(name) >= 0){
+    return callback(true);
+  } else {
+    return callback(false);
+  }
+
+}
 
 
 
@@ -88,6 +101,15 @@ contains(names, 'Colt', function(result){
 
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
+function uniq(arr, cb){
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    if(newArr.indexOf(arr[i]) === -1){
+      newArr.push(arr[i]);
+    }
+  }
+  return cb(newArr);
+}
 
 
 
@@ -104,7 +126,11 @@ uniq(names, function(uniqArr){
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
 
-
+function each(arr, cb){
+  for (var i = 0; i < arr.length; i++) {
+    cb(arr[i], i);
+  }
+}
 
     //Code Here for each
 
@@ -119,7 +145,13 @@ each(names, function(item, indice){
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
 
-
+function getUserById(array, value, cb) {
+    for(var i = 0; i < array.length; i += 1) {
+        if(array[i].id === value) {
+           cb(array[i]);    // Code Here
+        }
+    }
+}
 
 
  //code here for getUserById
@@ -146,5 +178,5 @@ var users = [
 ];
 
 getUserById(users, '16t', function(user){
-  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address);
 });
